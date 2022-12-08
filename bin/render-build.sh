8ba77bbda3aaf674ac15705896342320f684afca
rtm29331 <115970853+rtm29331@users.noreply.github.com>
2022-12-08 12:31:00 +0900

    depr

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,3 +6,7 @@ bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
+
+yarn build # jsファイルをesbuildでバンドルしているため
+bundle exec rake assets:precompile # cssはsprocketsを使っているため
+bundle exec ridgepole -c config/database.yml -E production --apply -f db/schemas/Schemafile 
